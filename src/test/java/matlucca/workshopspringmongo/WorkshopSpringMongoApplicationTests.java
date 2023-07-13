4524881dc0745c6933701c246797c07c6be17aab
MatheusLucca <mat.lualves@gmail.com>
2023-07-13 19:44:11 -0300

    Create Project

---
+++ src/test/java/matlucca/workshopspringmongo/WorkshopSpringMongoApplicationTests.java
@@ -0,0 +1,13 @@
+package matlucca.workshopspringmongo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkshopSpringMongoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
