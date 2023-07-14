b363ba2075b073eb32ecd3e78ef3829351005109
MatheusLucca <mat.lualves@gmail.com>
2023-07-13 22:29:39 -0300

    User findById

--- src/main/java/matlucca/workshopspringmongo/services/UserService.java
+++ src/main/java/matlucca/workshopspringmongo/services/UserService.java
@@ -2,6 +2,7 @@ package matlucca.workshopspringmongo.services;
 
 import matlucca.workshopspringmongo.entities.User;
 import matlucca.workshopspringmongo.repositories.UserRepository;
+import matlucca.workshopspringmongo.services.exceptions.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -16,5 +17,11 @@ public class UserService {
     public List<User> findAll(){
         return repository.findAll();
     }
+
+    public User findById(String id){
+        User user = repository.findById(id).orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado")); 
+        return user;
+    }
+
 
 }
---
+++ src/main/java/matlucca/workshopspringmongo/services/exceptions/ObjectNotFoundException.java
@@ -0,0 +1,8 @@
+package matlucca.workshopspringmongo.services.exceptions;
+
+public class ObjectNotFoundException extends RuntimeException{
+
+    public ObjectNotFoundException(String msg){
+        super(msg);
+    }
+}
