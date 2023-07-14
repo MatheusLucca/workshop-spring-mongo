b161babf472a675401dc46918fcc7c2db3ac177e
MatheusLucca <mat.lualves@gmail.com>
2023-07-13 23:39:38 -0300

    Create Posts findByTitle

--- src/main/java/matlucca/workshopspringmongo/controllers/PostController.java
+++ src/main/java/matlucca/workshopspringmongo/controllers/PostController.java
@@ -6,6 +6,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @RestController
 @RequestMapping(value = "/posts")
 public class PostController {
@@ -17,5 +19,11 @@ public class PostController {
     public ResponseEntity<Post> findById(@PathVariable String id){
         Post obj = service.findById(id);
         return ResponseEntity.ok().body(obj);
+    }
+
+    @GetMapping(value = "/titlesearch")
+    public ResponseEntity<List<Post>> findByTitle(@RequestParam(value = "text", defaultValue = "") String text){
+        List<Post> list = service.findByTitle(text);
+        return ResponseEntity.ok().body(list);
     }
 }
--- src/main/java/matlucca/workshopspringmongo/services/PostService.java
+++ src/main/java/matlucca/workshopspringmongo/services/PostService.java
@@ -6,6 +6,8 @@ import matlucca.workshopspringmongo.services.exceptions.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 
 @Service
 public class PostService {
@@ -16,5 +18,9 @@ public class PostService {
     public Post findById(String id){
         Post post = repository.findById(id).orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
         return post;
+    }
+
+    public List<Post> findByTitle(String text){
+        return repository.findByTitleContainingIgnoreCase(text);
     }
 }
