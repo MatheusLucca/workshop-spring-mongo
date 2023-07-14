7b127b9165beef9d92a9e460b20c52bcd9ceb4b9
MatheusLucca <mat.lualves@gmail.com>
2023-07-13 23:59:45 -0300

    Consult with multiple parameters

--- src/main/java/matlucca/workshopspringmongo/controllers/PostController.java
+++ src/main/java/matlucca/workshopspringmongo/controllers/PostController.java
@@ -1,11 +1,13 @@
 package matlucca.workshopspringmongo.controllers;
 
+import matlucca.workshopspringmongo.controllers.util.URL;
 import matlucca.workshopspringmongo.entities.Post;
 import matlucca.workshopspringmongo.services.PostService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.Date;
 import java.util.List;
 
 @RestController
@@ -28,7 +30,20 @@ public class PostController {
 
     @GetMapping(value = "/titlesearch")
     public ResponseEntity<List<Post>> findByTitle(@RequestParam(value = "text", defaultValue = "") String text){
+        text = URL.decodeParam(text);
         List<Post> list = service.findByTitle(text);
+        return ResponseEntity.ok().body(list);
+    }
+
+    @GetMapping(value = "/fullsearch")
+    public ResponseEntity<List<Post>> fullSearch(
+            @RequestParam(value = "text", defaultValue = "") String text,
+            @RequestParam(value = "minDate", defaultValue = "") String minDate,
+            @RequestParam(value = "maxDate", defaultValue = "") String maxDate){
+        text = URL.decodeParam(text);
+        Date min = URL.convertDate(minDate, new Date(0L));
+        Date max = URL.convertDate(maxDate, new Date());
+        List<Post> list = service.fullSearch(text, min, max);
         return ResponseEntity.ok().body(list);
     }
 }
--- src/main/java/matlucca/workshopspringmongo/services/PostService.java
+++ src/main/java/matlucca/workshopspringmongo/services/PostService.java
@@ -6,6 +6,7 @@ import matlucca.workshopspringmongo.services.exceptions.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.Date;
 import java.util.List;
 
 
@@ -25,5 +26,10 @@ public class PostService {
 
     public List<Post> findByTitle(String text){
         return repository.searchTitle(text);
+    }
+
+    public List<Post> fullSearch(String text, Date minDate, Date maxDate){
+        maxDate = new Date(maxDate.getTime() + 24 * 60 * 60 * 1000); // add 1 day to maxDate
+        return repository.fullSearch(text, minDate, maxDate);
     }
 }
