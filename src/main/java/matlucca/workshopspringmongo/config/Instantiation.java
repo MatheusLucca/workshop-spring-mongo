8c476e5e1d21d404832811eff95513da99895ae0
MatheusLucca <mat.lualves@gmail.com>
2023-07-13 23:07:33 -0300

    AuthorDTO

--- src/main/java/matlucca/workshopspringmongo/config/Instantiation.java
+++ src/main/java/matlucca/workshopspringmongo/config/Instantiation.java
@@ -1,5 +1,6 @@
 package matlucca.workshopspringmongo.config;
 
+import matlucca.workshopspringmongo.dto.AuthorDTO;
 import matlucca.workshopspringmongo.entities.Post;
 import matlucca.workshopspringmongo.entities.User;
 import matlucca.workshopspringmongo.repositories.PostRepository;
@@ -30,12 +31,12 @@ public class Instantiation implements CommandLineRunner {
         User u1 = new User(null, "Maria Brown", "maria@gmail.com");
         User u2 = new User(null, "Alex Green", "alexgreen@gmail.com");
         User u3 = new User(null, "Bob Grey", "bob@gmail.com");
-
-        Post p1 = new Post(null, sdf.parse("21/03/2021"), "Partiu viagem", "Vou viajar para São Paulo. Abraços!", u1);
-        Post p2 = new Post(null, sdf.parse("23/03/2021"), "Bom dia", "Acordei feliz hoje!", u1);
-
-
         userRepository.saveAll(List.of(u1, u2, u3));
+        Post p1 = new Post(null, sdf.parse("21/03/2021"), "Partiu viagem", "Vou viajar para São Paulo. Abraços!", new AuthorDTO(u1));
+        Post p2 = new Post(null, sdf.parse("23/03/2021"), "Bom dia", "Acordei feliz hoje!", new AuthorDTO(u1));
+
+
+
         postRepository.saveAll(List.of(p1, p2));
 
     }
