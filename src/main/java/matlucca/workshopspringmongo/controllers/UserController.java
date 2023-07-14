1390f5a110814ac9cdc79d0d8043834f5c903fb5
MatheusLucca <mat.lualves@gmail.com>
2023-07-13 21:56:29 -0300

    Connect MongoDB and create User repository and service

--- src/main/java/matlucca/workshopspringmongo/controllers/UserController.java
+++ src/main/java/matlucca/workshopspringmongo/controllers/UserController.java
@@ -1,6 +1,8 @@
 package matlucca.workshopspringmongo.controllers;
 
 import matlucca.workshopspringmongo.entities.User;
+import matlucca.workshopspringmongo.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -12,11 +14,12 @@ import java.util.List;
 @RequestMapping(value = "/users")
 public class UserController {
 
+    @Autowired
+    private UserService service;
     @GetMapping
     public ResponseEntity<List<User>> findAll(){
-        User maria = new User("1", "Maria Brown", "maria@gmail.com");
-        User alex = new User("2", "Alex Green", "alex@gmail.com");
-        return ResponseEntity.ok().body(List.of(maria, alex));
+
+        return ResponseEntity.ok().body(service.findAll());
 
     }
 }
--- src/main/java/matlucca/workshopspringmongo/entities/User.java
+++ src/main/java/matlucca/workshopspringmongo/entities/User.java
@@ -1,9 +1,13 @@
 package matlucca.workshopspringmongo.entities;
 
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
 import java.io.Serializable;
 import java.util.Objects;
-
+@Document
 public class User implements Serializable {
+    @Id
     private String id;
     private String name;
     private String email;
